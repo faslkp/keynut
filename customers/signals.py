cd60e1cbeb44f5d44c4efc7ad75240596d285108
Fasalu <faslkp@gmail.com>
2025-04-04 15:57:31 +0530

    but fix

---
+++ customers/signals.py
@@ -0,0 +1,12 @@
+import uuid
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def generate_referral_key(sender, instance, created, **kwargs):
+    if created and not instance.referral_key:  # Only for new users
+        instance.referral_key = uuid.uuid4()
+        instance.save()
